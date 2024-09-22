f1350a98ff93ed535f54ccad9fbd0f0400bcdaa1
avila-r <avila.dev@outlook.com>
2024-09-22 19:03:42 -0300

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello World!")
+	})
+
+	url := os.Getenv("SERVER_URL")
+
+	app.Listen(url)
+}
